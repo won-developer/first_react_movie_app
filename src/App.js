c26e7c76428a283a467bcde3ed883b36c92866d1
won-developer <gw4609@gmail.com>
2020-03-18 22:21:26 +0900

    routers

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { HashRouter, Route } from "react-router-dom";
+import About from "./routes/About";
+import Home from "./routes/Home";
 
 const App = () => {
-  return <div>hihihihi</div>;
+  return (
+    <HashRouter>
+      <Route path="/" exact={true} component={Home}></Route>
+      <Route path="/about" component={About}></Route>
+    </HashRouter>
+  );
 };
 
 export default App;
---
+++ src/routes/About.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const About = () => {
+  return <span>하이!</span>;
+};
+
+export default About;
